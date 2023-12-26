4707e435e16c45569c1704db90c5eac6412c3d11
Sarvesh Mayilvahanan <sarvesh.mayil@gmail.com>
2023-12-26 15:52:18 -0800

    fixed padding for conv modules

--- model/modules/conv.py
+++ model/modules/conv.py
@@ -1,10 +1,12 @@
 import torch
 import torch.nn as nn
 
+from .utils import autopad
+
 class Conv(nn.Module):
-    def __init__(self, in_channels:int, out_channels:int, kernel_size:int=3, stride:int=1, padding:int=0, bias:bool=False):
+    def __init__(self, in_channels:int, out_channels:int, kernel_size:int=3, stride:int=1, padding:int=None, bias:bool=False):
         super().__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding, bias=bias)
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=autopad(kernel_size, padding), bias=bias)
         self.bn = nn.BatchNorm2d(out_channels)
         self.act = nn.SiLU()
 
--- model/modules/utils.py
+++ model/modules/utils.py
@@ -1,6 +1,14 @@
 import torch
 
 
+def autopad(kernel_size:int, padding:int=None):
+    """
+    Calculate padding size automatically
+    """
+    if padding is None:
+        return kernel_size // 2 if isinstance(kernel_size, int) else [k // 2 for k in kernel_size]
+    return padding
+
 def dist2bbox(distance:torch.Tensor, anchor_points:torch.Tensor, xywh:bool=True, dim:int=-1):
     """
     Transform distance in (ltrb) to bounding box (xywh) or (xyxy)
