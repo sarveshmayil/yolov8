10de4609de523abd02fd7681e65186d8760d53cc
Sarvesh Mayilvahanan <sarvesh.mayil@gmail.com>
2024-01-05 18:15:25 -0800

    function to visualize dataset annotations

---
+++ model/visualization/view_dataset.py
@@ -0,0 +1,51 @@
+import yaml
+import os
+import cv2
+import numpy as np
+from matplotlib import colormaps as cm
+
+
+def visualize_dataset(config_file:str):
+    config_dict = yaml.safe_load(open(config_file, 'r'))
+
+    im_dir = os.path.join(config_dict['path'], config_dict['train'])
+    label_dir = os.path.join(config_dict['path'], config_dict['train_labels'])
+
+    num_classes = len(config_dict['names'])
+
+    cmap = cm['jet']
+
+    for im_file in os.listdir(im_dir):
+        id = os.path.splitext(im_file)[0]
+        image = cv2.imread(os.path.join(im_dir, im_file))
+        annotations = open(os.path.join(label_dir, id+'.txt'), 'r').readlines()
+
+        H, W = image.shape[:2]
+
+        for ann in annotations:
+            ann = ann.strip('\n').split(' ')
+            cls = int(ann[0])
+            label = config_dict['names'][cls]
+
+            # box provided in xywh format
+            xywh = np.array(ann[1:5], dtype=float)
+            xywh = xywh * [W, H, W, H]
+            xywh = xywh.astype(int)
+
+            x1, y1, x2, y2 = xywh[0]-xywh[2]//2, xywh[1]-xywh[3]//2, xywh[0]+xywh[2]//2, xywh[1]+xywh[3]//2
+
+            cls_color = cmap(cls/num_classes, bytes=True)[:3]
+            cls_color = (int(cls_color[0]), int(cls_color[1]), int(cls_color[2]))
+
+            # draw bounding box
+            cv2.rectangle(image, (x1, y1), (x2, y2), cls_color, 2)
+            
+            # draw label
+            (w, h), _ = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.6, 1)
+            cv2.rectangle(image, (x1, y1), (x1+w, y1-h), cls_color, -1)
+            cv2.putText(image, label, (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 0), 1, cv2.LINE_AA)
+
+        cv2.imshow('annotations', image)
+        cv2.waitKey(0)
+
+    cv2.destroyAllWindows()
